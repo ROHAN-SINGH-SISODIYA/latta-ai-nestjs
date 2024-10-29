38582a1daee96e76cdd31bd63ec63da847b1d4a0
ROHAN-SINGH-SISODIYA <41806939+ROHAN-SINGH-SISODIYA@users.noreply.github.com>
2024-10-30 00:24:41 +0530

    create issue

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,7 +18,7 @@ export class TasksService {
   }
 
   findOne(id: number): Task {
-    const task = this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.description === id);
     if (!task) throw new NotFoundException(`Task with ID ${id} not found`);
     return task;
   }
